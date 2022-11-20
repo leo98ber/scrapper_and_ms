7d0ef57d15e8bcceacfe4d44f22a4a3ebe6f2cb8
leo98ber <leonardoberoes94@gmail.com>
2022-11-20 12:25:39 -0400

    feat!: import errors module from other project

---
+++ scrapper/errors.py
@@ -0,0 +1,15 @@
+class InvalidParameters(Exception):
+    pass
+
+
+class DataNotFound(Exception):
+    pass
+
+
+class InvalidResponseData(Exception):
+    pass
+
+
+class UnexpectedErrorFromService(Exception):
+    pass
+
