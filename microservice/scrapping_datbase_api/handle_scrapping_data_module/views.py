53f55cfba42cb168629d3ebce059ac7d37750b0c
leo98ber <leonardoberoes94@gmail.com>
2022-11-20 12:05:46 -0400

    feat!: create viewset to access scrapping data

--- microservice/scrapping_datbase_api/handle_scrapping_data_module/views.py
+++ microservice/scrapping_datbase_api/handle_scrapping_data_module/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from general_views import GeneralViewSet
+from handle_scrapping_data_module.serializers import ScrappingDataSerializer
 
-# Create your views here.
+
+class ScrappingDataViewSet(GeneralViewSet):
+    serializer_class = ScrappingDataSerializer
