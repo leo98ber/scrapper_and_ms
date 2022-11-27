3dea52d6fcebd8593a2bf5154af2ca59611615cf
leo98ber <leonardoberoes94@gmail.com>
2022-11-27 10:32:27 -0400

    feat!: test some scrapping code

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,4 +1,8 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import random
+from time import sleep
 import json
 
 import os
@@ -26,15 +30,56 @@ def query_to_service(data):
 
     return True
 
+
+def find_ci(driver, document):
+    driver.find_element(By.XPATH, f'//option[@value="{document.upper()}"]').click()
+    id = driver.find_element(By.XPATH, '//input[@name="cedula"]')
+    id.clear()
+    cedula = int(input("\n\nIntroduzca su cedula: \n\n"))
+    id.send_keys(cedula)
+    sleep(random.uniform(1.0, 2.0))
+
+    button = driver.find_element(By.XPATH, '//input[@title="Buscar"]')
+    button.click()
+    print("Buscando")
+
+    sleep(random.uniform(2.0, 3.0))
+
+    register = driver.find_elements(By.XPATH, '//td[@align="left"]')
+
+    person_dates = []
+    dates = []
+
+    # assert len(
+    #     register) == 22, "No se encontraron resultados, esto puede deberse a que introdujo una cedula invalida o inexistente"
+
+    for date in register:
+        date_person = date.text
+        person_dates.append(date_person)
+
+    for i in range(1, 11, 2):
+        dates.append(person_dates[i])
+
+    print("\n\nDATOS\n\n", dates)
+    id.clear()
+
+    return {'name': cedula}
+
+
 def scrapper():
-    pass
+    driver = webdriver.Chrome()
+    driver.get('http://www.cne.gob.ve/web/index.php')
+    print("\n\nPagina lista\n\n")
+    sleep(random.uniform(1.0, 2.0))
+
+    input_ci = input("\n\nIndique que tipo de cedula posee?(V/E): ").lower()
+
+    return find_ci(driver, input_ci)
 
 
 if __name__ == '__main__':
     is_successful = None
     try:
-        params = sys.argv
-        # data = {'name': 'beroes'}
         data = scrapper()
         is_successful = query_to_service(data)
 
